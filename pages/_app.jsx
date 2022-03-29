54437848de2851db194a839b20f0524223b0fbf8
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-29 19:50:58 +0200

    landing page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
-import 'antd/dist/antd.css'
+import '../styles/globals.css';
+import 'antd/dist/antd.css';
+import 'antd-css-utilities/utility.min.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
