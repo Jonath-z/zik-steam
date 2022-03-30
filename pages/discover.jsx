e58efee575cdce9ea573bc1d4bbec5310abe9fb0
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-30 16:55:58 +0200

    descover page

---
+++ pages/discover.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DiscoverPage from '../components/Discover';
+
+const Discover = () => {
+  return (
+    <div>
+      <DiscoverPage />
+    </div>
+  );
+};
+
+export default Discover;
