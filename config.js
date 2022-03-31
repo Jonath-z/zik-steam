6b1f8bdc93e9cec24c70f4f2b8a1788e83bf4321
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-31 23:26:27 +0200

    contract address and ABI

---
+++ config.js
@@ -0,0 +1,223 @@
+export const marketAddress =
+  '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+export const market_ABI = [
+  {
+    inputs: [],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '_songId',
+        type: 'uint256',
+      },
+    ],
+    name: 'deleteSong',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'getListingFee',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '_songId',
+        type: 'uint256',
+      },
+    ],
+    name: 'getOneSong',
+    outputs: [
+      {
+        components: [
+          {
+            internalType: 'uint256',
+            name: 'id',
+            type: 'uint256',
+          },
+          {
+            internalType: 'string',
+            name: 'metadata',
+            type: 'string',
+          },
+          {
+            internalType: 'uint256',
+            name: 'price',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'supportPrice',
+            type: 'uint256',
+          },
+          {
+            internalType: 'address payable',
+            name: 'owner',
+            type: 'address',
+          },
+        ],
+        internalType: 'struct Market.Song',
+        name: '',
+        type: 'tuple',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'getSongs',
+    outputs: [
+      {
+        components: [
+          {
+            internalType: 'uint256',
+            name: 'id',
+            type: 'uint256',
+          },
+          {
+            internalType: 'string',
+            name: 'metadata',
+            type: 'string',
+          },
+          {
+            internalType: 'uint256',
+            name: 'price',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'supportPrice',
+            type: 'uint256',
+          },
+          {
+            internalType: 'address payable',
+            name: 'owner',
+            type: 'address',
+          },
+        ],
+        internalType: 'struct Market.Song[]',
+        name: '',
+        type: 'tuple[]',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'newListingFee',
+        type: 'uint256',
+      },
+    ],
+    name: 'modifyListingFee',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    name: 'songs',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: 'id',
+        type: 'uint256',
+      },
+      {
+        internalType: 'string',
+        name: 'metadata',
+        type: 'string',
+      },
+      {
+        internalType: 'uint256',
+        name: 'price',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'supportPrice',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address payable',
+        name: 'owner',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '_songId',
+        type: 'uint256',
+      },
+    ],
+    name: 'streamSong',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '_songId',
+        type: 'uint256',
+      },
+    ],
+    name: 'streamSongAsSupport',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'string',
+        name: '_metadata',
+        type: 'string',
+      },
+      {
+        internalType: 'uint256',
+        name: '_price',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: '_supportPrice',
+        type: 'uint256',
+      },
+    ],
+    name: 'uploadSong',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+];
