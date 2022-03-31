04a7757de944f2cf6d126dff9da28bb2d4953d6b
Jonath-z <jonathanzihindula95@gmail.com>
2022-03-31 23:23:00 +0200

    upload song handle in context

--- components/_Auth/UploadSong/index.jsx
+++ components/_Auth/UploadSong/index.jsx
@@ -1,46 +1,108 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import { Row, Col, Input, Button, Space } from 'antd';
-import { create as ipfsHttpClient } from 'ipfs-http-client';
+import { useUploadSong } from '../../contexts/UploadSongContext';
+import AudioPlayer from '../../modules/AudioPlayer';
+import ReactAudioPlayer from 'react-audio-player';
 
 const UploadSong = () => {
-  const [songMetadata, setSongMetadata] = useState({
-    coverUrl: '',
-    title: '',
-    artist: '',
-    genre: '',
-    label: '',
-  });
+  const {
+    onCoverfileChange,
+    onSongTitleChange,
+    onArtistLabelChange,
+    onSongArtistChange,
+    onSongGenreChange,
+    onSongPriceChange,
+    onSongSupportPriceChange,
+    createSong,
+    isReadyForUploading,
+    isSuccessfullyUploaded,
+    coverUrl,
+    setIsSuccessFullyUploaded,
+    songDataPreview,
+    onTrackfileChange,
+  } = useUploadSong();
 
-  const [isReadyForUploading, setIsReadyForUploading] =
-    useState(false);
-  const [coverPreview, setCoverPreview] = useState('');
+  isSuccessfullyUploaded &&
+    setTimeout(2000, setIsSuccessFullyUploaded(false));
+
+  const isSongPreviewReady = () => {
+    if (
+      !songDataPreview.artist ||
+      !songDataPreview.title ||
+      !songDataPreview.image ||
+      !songDataPreview.audioSrc
+    )
+      return false;
+    console.log(new Audio(songDataPreview.audioSrc));
+    return true;
+  };
 
   return (
-    <Row>
-      <Col>
-        {coverPreview && (
-          <Image src={coverPreview} alt="music-cover" />
-        )}
-      </Col>
-      <Col>
-        <Space direction="vertical">
-          <Input type="file" placeholder="Upload cover" />
-          <Input type="text" placeholder="Title" />
-          <Input type="text" placeholder="Arttis name" />
-          <Input type="text" placeholder="Genre (Optional)" />
-          <Input type="text" placeholder="label (Optional)" />
+    <>
+      {isSuccessfullyUploaded && <div>SuccessFully Uploaded </div>}
+      <Row>
+        <Col>
+          <Space direction="vertical">
+            <Input
+              type="file"
+              placeholder="Upload cover"
+              onChange={onTrackfileChange}
+            />
+            <Input
+              type="text"
+              placeholder="Title"
+              onChange={onSongTitleChange}
+            />
+            <Input
+              type="text"
+              placeholder="Arttis name"
+              onChange={onSongArtistChange}
+            />
+            <Input
+              type="text"
+              placeholder="Genre (Optional)"
+              onChange={onSongGenreChange}
+            />
+            <Input
+              type="text"
+              placeholder="label (Optional)"
+              onChange={onArtistLabelChange}
+            />
 
-          <Col>
-            <Space>
-              <Input type="number" placeholder="Price" />
-              <Input type="number" placeholder="Price for support" />
-            </Space>
-          </Col>
-          <Button>Post the Song</Button>
-        </Space>
-      </Col>
-    </Row>
+            <Col>
+              <Space>
+                <Input
+                  type="number"
+                  placeholder="Price"
+                  onChange={onSongPriceChange}
+                />
+                <Input
+                  type="number"
+                  placeholder="Price for support"
+                  onChange={onSongSupportPriceChange}
+                />
+              </Space>
+            </Col>
+            {isReadyForUploading && (
+              <Button onClick={createSong}>Post the Song</Button>
+            )}
+          </Space>
+          {isSongPreviewReady() && (
+            <AudioPlayer
+              tracks={[
+                {
+                  title: songDataPreview.title,
+                  image: songDataPreview.image,
+                  artist: songDataPreview.artist,
+                  audioSrc: songDataPreview.audioSrc,
+                },
+              ]}
+            />
+          )}
+        </Col>
+      </Row>
+    </>
   );
 };
 
