67547f896ba1e6af9f9e88d2a8a5eecce1d4ec8e
Jonath-z <jonathanzihindula95@gmail.com>
2022-04-04 19:38:59 +0200

    onClick event added to the button

--- components/modules/SignupButton/index.jsx
+++ components/modules/SignupButton/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Button } from 'antd';
+import { useRouter } from 'next/router';
 
 const SignupButton = () => {
+  const routes = useRouter();
   return (
     <div>
       <Button
         type="primary"
         style={{ background: '#00C3FF', color: '#000' }}
+        onClick={() => routes.push('/signup')}
       >
         SIGN UP
       </Button>
