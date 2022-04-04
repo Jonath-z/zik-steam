e7cfdaa01d96c3088d6ce3b32779a8ea6a987c62
Jonath-z <jonathanzihindula95@gmail.com>
2022-04-04 19:39:25 +0200

    env variable added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    CRYPTO_JS_KEY: process.env.CRYPTO_JS_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
