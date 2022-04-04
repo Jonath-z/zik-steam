efabdb0b7e30d88fa96ec1183ba3a5bf81f0eb89
Jonath-z <jonathanzihindula95@gmail.com>
2022-04-04 19:40:43 +0200

    signup page

---
+++ components/_NoAuth/Signup/index.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Input, Row, Col, Space, Button } from 'antd';
+import { useSignup } from '../../contexts/SingnupContext';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+
+const SignupPage = () => {
+  const {
+    onEmailChange,
+    onPasswordChange,
+    createAccount,
+    isCreationProcess,
+    isGettingError,
+    isValidPassword,
+    email,
+  } = useSignup();
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        // alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <p>Welcome.</p>
+      <p>Let&apos;s create your account</p>
+      <Row>
+        <Col>
+          <Space direction="vertical">
+            <Input
+              type="email"
+              placeholder="Enter your email"
+              onChange={onEmailChange}
+            />
+            <Input
+              type="password"
+              placeholder="Password"
+              onChange={onPasswordChange}
+              style={{
+                borderColor: `${isValidPassword ? 'blue' : 'red'}`,
+              }}
+            />
+            {isGettingError && (
+              <p styles={{ color: 'red' }}>
+                <b>{email}</b> exists already, please change this
+                email
+              </p>
+            )}
+            <Button
+              onClick={createAccount}
+              disabled={isValidPassword ? false : true}
+            >
+              {isCreationProcess ? (
+                <span>
+                  <AiOutlineLoading3Quarters /> Processing
+                </span>
+              ) : (
+                <span>Create an account</span>
+              )}
+            </Button>
+            <p>Log in Now</p>
+          </Space>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default SignupPage;
