7dc173d71e096fb90d92b1062d41b45a59722b64
Jonath-z <jonathanzihindula95@gmail.com>
2022-04-06 02:16:22 +0200

    _auth folder changed into  secured

---
+++ components/_secured/UploadSong/index.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from 'react';
+import { Row, Col, Input, Button, Space } from 'antd';
+import { useUploadSong } from '../../contexts/UploadSongContext';
+import AudioPlayer from '../../modules/AudioPlayer';
+
+const UploadSong = () => {
+  const {
+    onCoverFileChange,
+    onSongTitleChange,
+    onArtistLabelChange,
+    onSongArtistChange,
+    onSongGenreChange,
+    onSongPriceChange,
+    onSongSupportPriceChange,
+    createSong,
+    isSuccessfullyUploaded,
+    isCorrectImageSize,
+    songDataPreview,
+    onTrackfileChange,
+  } = useUploadSong();
+
+  const isSongPreviewReady = () => {
+    if (
+      !songDataPreview.artist ||
+      !songDataPreview.title ||
+      !songDataPreview.image ||
+      !songDataPreview.audioSrc
+    )
+      return false;
+    return true;
+  };
+
+  return (
+    <>
+      {isSuccessfullyUploaded && <div>SuccessFully Uploaded </div>}
+      <Row className="flex flex-col justify-center">
+        <Col>
+          <Space direction="vertical">
+            <label htmlFor="song">Song </label>
+            <Input
+              type="file"
+              name="song"
+              placeholder="Upload Song"
+              onChange={onTrackfileChange}
+            />
+            <label htmlFor="cover">Cover </label>
+            <Input
+              type="file"
+              name="cover"
+              placeholder="Upload Song"
+              onChange={onCoverFileChange}
+            />
+            {!isCorrectImageSize && (
+              <p>Song cover must have a square size</p>
+            )}
+            <label htmlFor="title">Song Title </label>
+            <Input
+              type="text"
+              name="title"
+              placeholder="Title"
+              onChange={onSongTitleChange}
+            />
+            <label htmlFor="artist">Artist name</label>
+            <Input
+              type="text"
+              name="arist"
+              placeholder="Arttis name"
+              onChange={onSongArtistChange}
+            />
+            <label htmlFor="genre">Genre</label>
+            <Input
+              type="text"
+              name="genre"
+              placeholder="Genre (Optional)"
+              onChange={onSongGenreChange}
+            />
+            <label htmlFor="label">Artist Lalbel</label>
+            <Input
+              type="text"
+              name="label"
+              placeholder="label (Optional)"
+              onChange={onArtistLabelChange}
+            />
+
+            <Col>
+              <Space>
+                <Row>
+                  <Space direction="vertical">
+                    <label htmlFor="price">Steaming Price</label>
+                    <Input
+                      type="number"
+                      name="price"
+                      placeholder="Price"
+                      onChange={onSongPriceChange}
+                    />
+                  </Space>
+                </Row>
+                <Row>
+                  <Space direction="vertical">
+                    <label htmlFor="supportPrice">Support</label>
+                    <Input
+                      type="number"
+                      name="supportPrice"
+                      placeholder="Price for support"
+                      onChange={onSongSupportPriceChange}
+                    />
+                  </Space>
+                </Row>
+              </Space>
+            </Col>
+            {<Button onClick={createSong}>Post the Song</Button>}
+          </Space>
+        </Col>
+      </Row>
+      {isSongPreviewReady() && (
+        <AudioPlayer
+          tracks={[
+            {
+              title: songDataPreview.title,
+              image: songDataPreview.image,
+              artist: songDataPreview.artist,
+              audioSrc: songDataPreview.audioSrc,
+            },
+          ]}
+        />
+      )}
+    </>
+  );
+};
+
+export default UploadSong;
