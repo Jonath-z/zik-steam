c5296ed873b89572abeafdbe8cf1d3c299e1dd8e
Jonath-z <jonathanzihindula95@gmail.com>
2022-04-08 17:23:47 +0200

    unused packages removed'

--- components/_NoAuth/Home/index.jsx
+++ components/_NoAuth/Home/index.jsx
@@ -4,17 +4,10 @@ import LayoutSider from './LayoutSider';
 import LayoutHeader from './LayoutHeader';
 import LayoutContent from './LayoutContent';
 import UploadSong from '../../_secured/UploadSong';
-import PropTypes from 'prop-types';
-import { useRouter } from 'next/router';
 import Discover from '../_modules/Discover';
 
-const HomePage = ({ isSecured }) => {
+const HomePage = () => {
   const [content, setContent] = useState(<div />);
-  const route = useRouter();
-  const { id } = route.query;
-  console.log(id);
-
-  console.log(isSecured);
 
   const setMenuContent = (menu) => {
     switch (menu) {
@@ -31,20 +24,13 @@ const HomePage = ({ isSecured }) => {
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <LayoutSider
-        isSecured={isSecured}
-        setMenuContent={setMenuContent}
-      />
+      <LayoutSider setMenuContent={setMenuContent} />
       <Layout>
         <LayoutHeader />
         <LayoutContent>{content}</LayoutContent>
       </Layout>
     </Layout>
   );
-};
-
-HomePage.propTypes = {
-  isSecured: PropTypes.bool.isRequired,
 };
 
 export default HomePage;
