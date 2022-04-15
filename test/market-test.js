58cd8fed35bfe2ceafd88ed3cb9f5958f275e6a9
Jonath-z <jonathanzihindula95@gmail.com>
2022-04-15 19:15:28 +0200

    test modified

--- test/market-test.js
+++ test/market-test.js
@@ -1,44 +1,45 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
-describe("Market", function () {
-  it("should create and execute the market contract", async () => {
-    const Market = await ethers.getContractFactory("Market");
+describe('Market', function () {
+  it('should create and execute the market contract', async () => {
+    const Market = await ethers.getContractFactory('Market');
     const market = await Market.deploy();
     await market.deployed();
     const marketAddress = market.address;
-    console.log("market address", marketAddress);
+    console.log('market address', marketAddress);
 
     let listingFee = await market.getListingFee();
     listingFee = listingFee.toString();
-    console.log("listing Fee", listingFee);
+    console.log('listing Fee', listingFee);
 
-    const auctionPrice = ethers.utils.parseUnits("2", "ether");
-    const supportAuctionPrice = ethers.utils.parseUnits("4", "ether");
+    const auctionPrice = ethers.utils.parseUnits('2', 'ether');
+    const supportAuctionPrice = ethers.utils.parseUnits('4', 'ether');
 
     await market.uploadSong(
-      "https://meta.com.io",
-      "https://stream.ipfs.io.thebloog",
+      'https://meta.com.io',
+      'https://stream.ipfs.io.thebloog',
       auctionPrice,
       supportAuctionPrice,
-      {
-        value: listingFee
-      });
+      '0x0',
+    );
 
     let songs = await market.getSongs();
-    songs = await Promise.all(songs.map(async (_song) => {
-      const song = {
-        id: _song.id,
-        metadata: _song.metadata,
-        price: _song.price,
-        supportPrice: _song.supportPrice,
-        owner: _song.owner
-      }
-      return song;
-    }));
-    console.log("songs", songs);
+    songs = await Promise.all(
+      songs.map(async (_song) => {
+        const song = {
+          id: _song.id,
+          metadata: _song.metadata,
+          price: _song.price,
+          supportPrice: _song.supportPrice,
+          owner: _song.owner,
+        };
+        return song;
+      }),
+    );
+    console.log('songs', songs);
 
-    let song = await market.getOneSong(1);
+    let song = await market.getSongById(1);
     console.log('my song', song);
 
     await market.deleteSong(1);
