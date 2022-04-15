9c0216de882fb65ac4c35aed7727890a42c71d6b
Jonath-z <jonathanzihindula95@gmail.com>
2022-04-15 19:07:59 +0200

    UI done

--- components/_NoAuth/Signup/index.jsx
+++ components/_NoAuth/Signup/index.jsx
@@ -3,6 +3,7 @@ import { Input, Row, Col, Space, Button } from 'antd';
 import { useSignup } from '../../contexts/SingnupContext';
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { useRouter } from 'next/router';
+import styles from '../../../styles/auth.module.css';
 
 const SignupPage = () => {
   const {
@@ -18,31 +19,29 @@ const SignupPage = () => {
 
   return (
     <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        // alignItems: 'center',
-        height: '100vh',
-      }}
+      className={`flex flex-col justify-center items-center h-screen w-full ${styles.loginBg}`}
     >
-      <p>Welcome.</p>
-      <p>Let&apos;s create your account</p>
+      <p className="text-4xl m-0 text-white">Zik-stream</p>
+      <p className="text-xl m-0 text-white">
+        Let&apos;s create your account
+      </p>
       <Row>
         <Col>
-          <Space direction="vertical">
-            <Input
+          <Space
+            direction="vertical"
+            className="w-96 my-10 text-center"
+          >
+            <input
               type="email"
               placeholder="Enter your email"
               onChange={onEmailChange}
+              className={`w-full py-2 px-3 bg-transparent border-white border outline-none rounded-md text-white placeholder:text-gray-500`}
             />
-            <Input
+            <input
               type="password"
               placeholder="Password"
               onChange={onPasswordChange}
-              style={{
-                borderColor: `${isValidPassword ? 'blue' : 'red'}`,
-              }}
+              className={`w-full py-2 px-3 bg-transparent border-white border outline-none rounded-md text-white placeholder:text-gray-500`}
             />
             {isGettingError && (
               <p styles={{ color: 'red' }}>
@@ -50,23 +49,28 @@ const SignupPage = () => {
                 email
               </p>
             )}
-            <Button
+            <button
               onClick={createAccount}
               disabled={isValidPassword ? false : true}
+              className={`w-full mt-4 py-2 rounded-md bg-white ${
+                isValidPassword ? 'bg-opacity-100' : 'bg-opacity-50'
+              } transition-[500ms]`}
             >
               {isCreationProcess ? (
-                <span>
-                  <AiOutlineLoading3Quarters /> Processing
+                <span className="flex justify-center items-center">
+                  <AiOutlineLoading3Quarters className="animate-spin mx-3" />{' '}
+                  Processing
                 </span>
               ) : (
                 <span>Create an account</span>
               )}
-            </Button>
+            </button>
             <p
-              className="cursor-pointer"
+              className="cursor-pointer text-white"
               onClick={() => routes.push('/login')}
             >
-              Log in Now
+              Do you have an account ?{' '}
+              <span className="text-blue-400">Log in Now</span>
             </p>
           </Space>
         </Col>
