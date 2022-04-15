cd73d54f0cf1ad20913496d4306b553d06258e20
Jonath-z <jonathanzihindula95@gmail.com>
2022-04-15 19:15:05 +0200

    UploadProvider removed

--- pages/home/[id].jsx
+++ pages/home/[id].jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import HomePage from '../../components/_NoAuth/Home';
-import UploadSongProvider from '../../components/contexts/UploadSongContext';
 import DiscoverProvider from '../../components/contexts/DiscoverContext';
 import AudioPlayerProvider from '../../components/contexts/AudioPlayerContext';
-import { useRouter } from 'next/router';
 import StreamProvider from '../../components/contexts/StreamContext';
+import UserProvider from '../../components/contexts/UserContext';
 
 const Discover = () => {
-  const route = useRouter();
-  const { id } = route.query;
-  console.log('my user id', id);
-
   return (
-    <UploadSongProvider id={id}>
+    <UserProvider>
       <DiscoverProvider>
         <AudioPlayerProvider>
           <StreamProvider>
@@ -20,7 +15,7 @@ const Discover = () => {
           </StreamProvider>
         </AudioPlayerProvider>
       </DiscoverProvider>
-    </UploadSongProvider>
+    </UserProvider>
   );
 };
 
