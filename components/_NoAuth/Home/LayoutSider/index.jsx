998147507e7f1effcf05a1698dd11b5da90b9d89
Jonath-z <jonathanzihindula95@gmail.com>
2022-04-15 19:06:50 +0200

    upload and account menu removed

--- components/_NoAuth/Home/LayoutSider/index.jsx
+++ components/_NoAuth/Home/LayoutSider/index.jsx
@@ -2,40 +2,13 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { Layout, Menu } from 'antd';
 import styles from '../../../../styles/Discover.module.css';
 import PropTypes from 'prop-types';
-import { useRouter } from 'next/router';
-import axios from 'axios';
 
 const { Sider } = Layout;
 const { Item, SubMenu } = Menu;
 
 const LayoutSider = ({ setMenuContent }) => {
-  const [isSecured, setIsSecured] = useState(false);
-  const route = useRouter();
-  const { id } = route.query;
-
-  const getuser = useCallback(async () => {
-    if (id) {
-      const response = await axios.post('/api/query/getUser', {
-        id: id,
-      });
-      console.log(response);
-      if (response.data.status === 302) {
-        setIsSecured(true);
-      } else {
-        setIsSecured(false);
-      }
-    } else {
-      setIsSecured(false);
-    }
-  }, [id]);
-
-  () => {
-    getuser();
-  },
-    [getuser];
-
   return (
-    <Sider theme="light" className={styles.siteLayoutSider}>
+    <Sider theme="light" className={`${styles.siteLayoutSider}`}>
       <div className="logo">
         <h1 className="text-black">Zik-Stream</h1>
       </div>
@@ -68,41 +41,21 @@ const LayoutSider = ({ setMenuContent }) => {
           >
             <span>Artists</span>
           </Item>
-          {isSecured && (
-            <Item
-              key="4"
-              style={{ paddingLeft: '48px' }}
-              onClick={() => setMenuContent('upload')}
-            >
-              <span>Upload Song</span>
-            </Item>
-          )}
-          {isSecured && (
-            <Item
-              key="5"
-              style={{ paddingLeft: '48px' }}
-              onClick={() => setMenuContent('account')}
-            >
-              <span>Account</span>
-            </Item>
-          )}
         </SubMenu>
-        {isSecured && (
-          <SubMenu key="sub2" title="Library">
-            <Item
-              key="6"
-              onClick={() => setMenuContent('recently played')}
-            >
-              <span>Recently Played</span>
-            </Item>
-            <Item
-              key="7"
-              onClick={() => setMenuContent('favorite tracks')}
-            >
-              <span>Favorite Tracks</span>
-            </Item>
-          </SubMenu>
-        )}
+        <SubMenu key="sub2" title="Library">
+          <Item
+            key="6"
+            onClick={() => setMenuContent('recently played')}
+          >
+            <span>Recently Played</span>
+          </Item>
+          <Item
+            key="7"
+            onClick={() => setMenuContent('favorite tracks')}
+          >
+            <span>Favorite Tracks</span>
+          </Item>
+        </SubMenu>
       </Menu>
     </Sider>
   );
