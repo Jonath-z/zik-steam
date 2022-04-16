6dd8b13bae1a65c27238f658fd7d2494e58fbb82
Jonath-z <jonathanzihindula95@gmail.com>
2022-04-16 18:33:47 +0200

    admin crate artist page done

--- components/admin/CreateArtist/index.jsx
+++ components/admin/CreateArtist/index.jsx
@@ -1,34 +1,73 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { Row, Col, Input, Button, Space } from 'antd';
+import client from '../../utils/helpers/ipfs-client';
+import axios from 'axios';
 
 const CreateArtist = () => {
+  const [artistIds, setArtistIds] = useState({
+    artistName: '',
+    artistProfileUrl: '',
+  });
+  const [progress, setProgress] = useState(0);
+
+  const calulProgress = (fileSize, Currentprogress) => {
+    const progress = (Currentprogress * 100) / fileSize;
+    return progress;
+  };
+
+  const onProfilePhotoChange = async (e) => {
+    const file = e.target.files[0];
+    console.log(file);
+    const added = await client.add(file, {
+      progress: (prog) => setProgress(calulProgress(file.size, prog)),
+    });
+    const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+    setArtistIds({ ...artistIds, artistProfileUrl: url });
+  };
+
+  const uploadArtist = async () => {
+    if (!artistIds.artistName || !artistIds.artistProfileUrl) return;
+    const response = await axios.post('/api/upload/uploadArtist', {
+      artistIds,
+    });
+    if (response.status === 200) {
+      window.alert('Artist uploaded');
+      setArtistIds({
+        artistName: '',
+        artistProfileUrl: '',
+      });
+      setProgress(0);
+    } else {
+      alert('error accurred when uploading the artist');
+    }
+  };
+
   return (
     <Row className="flex flex-col justify-center">
       <Col>
         <Space direction="vertical">
-          <label htmlFor="cover">Artist Photos </label>
+          <label htmlFor="profile-photo">Artist profile photo</label>
           <Input
             type="file"
-            name="cover"
+            name="profile-photo"
             placeholder="Upload Song"
-            onChange={onCoverFileChange}
+            onChange={onProfilePhotoChange}
             className={`bg-[#00C3FF] bg-opacity-[${coverUploadProgress}]`}
           />
-          <label htmlFor="title">Song Title </label>
+          {progress && <progress max={100} value={progress} />}
+          <label htmlFor="artist-name">Artist name</label>
           <Input
             type="text"
-            name="title"
+            name="artist-name"
             placeholder="Title"
-            onChange={onSongTitleChange}
+            onChange={(e) =>
+              setArtistIds({
+                ...artistIds,
+                artistName: e.target.value,
+              })
+            }
           />
-          <label htmlFor="artist">Artist name</label>
-          <Input
-            type="text"
-            name="arist"
-            placeholder="Arttis name"
-            onChange={onSongArtistChange}
-          />
-          <label htmlFor="genre">Genre</label>
+          <Button onClick={uploadArtist}>Upload Artist</Button>
         </Space>
       </Col>
     </Row>
