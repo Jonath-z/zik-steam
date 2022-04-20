8fcf39d7cb64ed460e59319092151d376ab2828c
Jonath-z <jonathanzihindula95@gmail.com>
2022-04-20 18:38:14 +0200

    favorite tracks API route

---
+++ pages/api/song/getfavoriteSongs.js
@@ -0,0 +1,17 @@
+import { db } from '../database/mongodb';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST' && req.body.userId) {
+    const user = await db
+      .collection('users')
+      .find({ id: req.body.userId })
+      .toArray();
+    console.log('user id in get all ', user);
+
+    const userFavoriteSongs = user[0].favorites;
+
+    res.status(200).json({
+      favoriteSongs: userFavoriteSongs,
+    });
+  }
+}
