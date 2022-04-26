68b0fda01d5383d0a96074770cd76cf4ed2f429b
Jonath-z <jonathanzihindula95@gmail.com>
2022-04-26 21:42:05 +0200

    customs audio controls deleted

---
+++ components/modules/AudioControls/PlayPauseButton.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import icons from '../../icons';
+import PropTypes from 'prop-types';
+
+const PlayPauseButton = ({
+  isPlaying,
+  setTracks,
+  //   track,
+}) => {
+  const { Pause, Play } = icons;
+  return (
+    <div className="flex justify-center items-center">
+      {isPlaying ? (
+        <button
+          type="button"
+          className="pause"
+          onClick={() => {}}
+          aria-label="Pause"
+        >
+          <Pause className="text-4xl" />
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="play"
+          onClick={() => {
+            setTracks();
+          }}
+          aria-label="Play"
+        >
+          <Play className="text-4xl" />
+        </button>
+      )}
+    </div>
+  );
+};
+
+PlayPauseButton.propTypes = {
+  isPlaying: PropTypes.bool.isRequired,
+  setTracks: PropTypes.func,
+  track: PropTypes.object,
+};
+
+export default PlayPauseButton;
