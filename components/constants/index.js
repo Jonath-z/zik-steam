7ffddf6341c8453e407062e2915e7e5516f07b47
Jonath-z <jonathanzihindula95@gmail.com>
2022-04-28 06:04:26 +0200

    theme constants added

---
+++ components/constants/index.js
@@ -0,0 +1,4 @@
+export const DARK_MODE_PRIMARY = '#000c17';
+export const DARK_MODE_SECONDARY = '#001529';
+export const BLUE_PRIMARY = '#1890ff';
+export const LAYOUT_CONTENT_LIGHT_MODE = '#e0e0e0';
