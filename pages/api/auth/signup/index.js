48aefca175ddff0f95a39a5921c9f4cfc2b051e5
Jonath-z <jonathanzihindula95@gmail.com>
2022-05-03 12:12:42 +0200

    synchronuis signup issue fixed

--- pages/api/auth/signup/index.js
+++ pages/api/auth/signup/index.js
@@ -1,17 +1,17 @@
 import { db } from '../../database/mongodb';
 
 export default async function handler(req, res) {
-  const isUserExits = async (email) => {
-    const user = await db
-      .collection('users')
-      .find({ email })
-      .toArray();
-    console.log('users', user.length);
-    if (user.length === 0) return false;
-    return true;
-  };
-
   if (req.method === 'POST') {
+    const isUserExits = async (email) => {
+      const user = await db
+        .collection('users')
+        .find({ email })
+        .toArray();
+      console.log('users', user.length);
+      if (user.length === 0) return false;
+      return true;
+    };
+
     console.log(req.body);
     const body = req.body;
     if (!(await isUserExits(body.email))) {
